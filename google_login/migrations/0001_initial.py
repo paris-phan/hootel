6386b6e799cd3be8ad13e9d0f70ed7a0cc54c9a6
Paris Phan <37759794+paris-phan@users.noreply.github.com>
2025-02-25 16:28:16 -0500

    Update 0001_initial.py

--- google_login/migrations/0001_initial.py
+++ google_login/migrations/0001_initial.py
@@ -15,7 +15,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='UserProfile',
             name="UserProfile",
             fields=[
                 (
