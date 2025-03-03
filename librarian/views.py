dddf010506a9f4021094f9a5225f914f87e4bc9f
AstralPhoenix01 <cardinalphoenix940@gmail.com>
2025-03-03 16:07:32 -0500

    Added Librarian feature for creating hotels

--- librarian/views.py
+++ librarian/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.utils import timezone
 # Create your views here.
 
 from django.contrib.auth.decorators import login_required
-
+from hotelmanager6000.models import Hotel
 def create_hotel(request):
+    if request.method == "POST":
+        name = request.POST["name_field"]
+        location = request.POST["location_field"]
+        print(name, location)
+        hotel = Hotel(name = name, location = location, rating = 0, created_at = timezone.now())
+        hotel.save()
+        return redirect('/')
     return render(request, 'librarian/create_hotel.html')
 @login_required
 def librarian_dashboard(request):
