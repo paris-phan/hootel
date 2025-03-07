58d51c18bde747c04c3a2549c2a04aa353b704e6
AstralPhoenix01 <cardinalphoenix940@gmail.com>
2025-03-07 13:12:31 -0500

    Revised 'owner' attribute to be a foreign key for the user

--- hotelmanager6000/models.py
+++ hotelmanager6000/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Hotel(models.Model):
     name = models.CharField(max_length=255)
-    owner = models.CharField(max_length=255, default = "")
+    owner = models.ForeignKey(User, on_delete = models.CASCADE)
     location = models.CharField(max_length=255)
     rating = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
