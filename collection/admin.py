530cdaab803f7743e093a5c81fbe17ebcc19f890
paris-phan <37759794+paris-phan@users.noreply.github.com>
2025-04-12 19:27:01 -0400

    base collections & catalog imp.

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Collection, CollectionItems, CollectionAuthorizedUser
 
-# Register your models here.
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'visibility', 'created_at', 'updated_at')
+    list_filter = ('visibility', 'created_at')
+    search_fields = ('title', 'description', 'creator__username')
+    date_hierarchy = 'created_at'
+
+@admin.register(CollectionItems)
+class CollectionItemsAdmin(admin.ModelAdmin):
+    list_display = ('collection', 'item')
+    list_filter = ('collection',)
+    search_fields = ('collection__title', 'item__title')
+
+@admin.register(CollectionAuthorizedUser)
+class CollectionAuthorizedUserAdmin(admin.ModelAdmin):
+    list_display = ('collection', 'user')
+    list_filter = ('collection',)
+    search_fields = ('collection__title', 'user__username')
