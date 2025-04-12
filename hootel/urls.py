d729fe672a7b556afd85dd625ba35c79622a1a75
paris-phan <37759794+paris-phan@users.noreply.github.com>
2025-04-12 18:29:54 -0400

    basic views setup

--- hootel/urls.py
+++ hootel/urls.py
@@ -20,4 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('core.urls')),
 ]
+
+handler404 = 'core.views.handler404'
+handler500 = 'core.views.handler500'
