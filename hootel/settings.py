28b8c79af77561633da41276045fcbbfa076bde3
Paris Phan <37759794+paris-phan@users.noreply.github.com>
2025-09-19 19:42:54 -0400

    Update settings.py

--- hootel/settings.py
+++ hootel/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["*", '.vercel.app', 'hoo-tels.com']
+ALLOWED_HOSTS = ["*", '.vercel.app', 'hoo-tels.com', '.hoo-tels.com']
 
 
 
